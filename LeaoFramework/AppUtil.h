61d6ee6a6d95f476709ef624254fefea1f1c8f53
李海博 <lihaibo@izaodao.com>
2020-03-11 14:06:34 +0800

    commit

---
+++ LeaoFramework/AppUtil.h
@@ -0,0 +1,17 @@
+//
+//  AppUtil.h
+//  LeaoFramework
+//
+//  Created by leao on 2020/3/11.
+//  Copyright © 2020 Zaodao(Dalian) Technology Co. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppUtil : NSObject
++ (void)showToast;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LeaoFramework/LeaoFramework.h
@@ -0,0 +1,20 @@
+//
+//  LeaoFramework.h
+//  LeaoFramework
+//
+//  Created by leao on 2020/3/11.
+//  Copyright © 2020 Zaodao(Dalian) Technology Co. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <LeaoFramework/AppUtil.h>
+
+//! Project version number for LeaoFramework.
+FOUNDATION_EXPORT double LeaoFrameworkVersionNumber;
+
+//! Project version string for LeaoFramework.
+FOUNDATION_EXPORT const unsigned char LeaoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeaoFramework/PublicHeader.h>
+
+
